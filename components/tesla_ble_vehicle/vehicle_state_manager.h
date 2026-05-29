0ba3ae715218abd4001147529ac08a65b90c91bd
Kim Biesbjerg <kim@medware.dk>
2026-05-29 18:20:07 +0200

    Keep charge accessors minimal

--- components/tesla_ble_vehicle/vehicle_state_manager.h
+++ components/tesla_ble_vehicle/vehicle_state_manager.h
@@ -123,8 +123,6 @@ public:
     bool is_user_present() const;
     bool is_charge_flap_open() const;
     bool is_charging() const { return is_charging_; }
-    bool has_charging_amps() const;
-    bool has_charging_limit() const;
     float get_charging_amps() const;
     float get_charging_limit() const;
     
